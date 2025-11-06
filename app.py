223efce45a7687c6705ab2d0912c19709415b892
Oluwatosin Saibu <olusai@nmsu.edu>
2025-11-06 16:54:27 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Alanine Dipeptide FES Tutorial")
+
+st.markdown("""
+Welcome!
+
+This Streamlit app will guide you through:
+1. Setting up REMD for alanine dipeptide in vacuum.
+2. Generating φ/ψ data.
+3. Uploading FES / COLVAR / dihedral files.
+4. Visualizing 2D and 1D free energy landscapes.
+
+Next step: we'll add upload + plotting.
+""")
